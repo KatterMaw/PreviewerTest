df2a794b1cb844591338ea8a38298247c300abfa
KatterMaw <narcat99@gmail.com>
2023-12-25 14:43:56 +0500

    Initial

---
+++ Theme/MyTheme.axaml.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace Theme;
+
+public sealed class MyTheme : Styles
+{
+	public MyTheme(IServiceProvider? serviceProvider = null)
+	{
+		AvaloniaXamlLoader.Load(serviceProvider, this);
+	}
+}
